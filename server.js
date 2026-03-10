1bcf2611e428617879fbd5753ff2803d7908cfd7
Suroopika0300 <suroopikamuthuraj@gmail.com>
2026-03-10 11:17:49 +0530

    fix cors

--- server.js
+++ server.js
@@ -17,11 +17,16 @@ import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(cors({
+    origin: ["https://electricial-frontend-kcad.vercel.app", "http://localhost:5173", "http://localhost:5000"],
+    credentials: true,
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
